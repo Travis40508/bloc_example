83e1db6e0d7ba037f56801873cd3f402854b5be5
Travis <rtressler@mgmresorts.com>
2023-01-14 12:52:19 -0800

    fixed issue that prevented me from using isA<> and 'having'.

--- test/dog_image_bloc_test.dart
+++ test/dog_image_bloc_test.dart
@@ -3,6 +3,7 @@ import 'package:bloc_example_tutorial/screens/bloc/dog_image_events.dart';
 import 'package:bloc_example_tutorial/screens/bloc/dog_image_states.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import 'mocks.dart';
 
@@ -19,8 +20,8 @@ void main() {
       bloc.add(FetchDogImage());
     },
     expect: () => [
-      DogImageLoadingState(),
-      DogImageErrorState(),
+      isA<DogImageLoadingState>(),
+      isA<DogImageErrorState>(),
     ],
   );
 
@@ -28,14 +29,22 @@ void main() {
     'On Success',
     build: () => DogImageBloc(repository: repo),
     setUp: () {
-      when(() => repo.fetchRandomDogImage()).thenAnswer((_) => Future.value('testResponse'));
+      when(() => repo.fetchRandomDogImage())
+          .thenAnswer((_) => Future.value('testResponse'));
     },
     act: (bloc) {
       bloc.add(FetchDogImage());
     },
     expect: () => [
-      DogImageLoadingState(),
-      DogImageLoadedState(imageUrl: 'testResponse'),
+      isA<DogImageLoadingState>(),
+      // having is nice because it allows us to determine WHICH (if any)
+      // variables we want to check against (rather than all of them, if you
+      // just use a constructor like we were before).
+      isA<DogImageLoadedState>().having(
+        (state) => state.imageUrl,
+        'has correct value for imageUrl',
+        'testResponse',
+      )
     ],
   );
 }
